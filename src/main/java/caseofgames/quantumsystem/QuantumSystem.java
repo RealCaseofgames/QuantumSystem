dac9bc6a9e3cf4d20deeee1ba4cc31fe1dc0cbc1
Caseofgames <caseofgames@gmail.com>
2021-02-09 07:57:52 -0600

    Modified it to move BlockRegistryEvent to another class

--- src/main/java/caseofgames/quantumsystem/QuantumSystem.java
+++ src/main/java/caseofgames/quantumsystem/QuantumSystem.java
@@ -1,81 +1,52 @@
-package caseofgames.quantummod;
+package caseofgames.quantumsystem;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("quantumsystem")
+import caseofgames.quantumsystem.client.ProxyClient;
+import caseofgames.quantumsystem.common.ProxyCommon;
+import caseofgames.quantumsystem.util.ForgeLoggerTweaker;
+
+@Mod(QuantumSystem.MODID)
 public class QuantumSystem
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MODID = "quantumsystem";
 
-    public QuantumSystem() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+	public static IEventBus MOD_EVENT_BUS;
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+	public QuantumSystem()
+	{
+		final boolean HIDE_CONSOLE_EXTRA = true;
+		if (HIDE_CONSOLE_EXTRA)
+		{
+			ForgeLoggerTweaker.setMinimumLevel(Level.WARN);
+			ForgeLoggerTweaker.applyLoggerFilter();
+		}
+		
+		MOD_EVENT_BUS = FMLJavaModLoadingContext.get().getModEventBus();
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        
-    }
-
-	private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+		MinecraftForge.EVENT_BUS.register(this);
+		
+		registerCommonEvents();
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> registerClientOnlyEvents());
+	}
+	
+	public static void registerCommonEvents()
+	{
+		MOD_EVENT_BUS.register(ProxyCommon.class);
+	}
+	
+	public static void registerClientOnlyEvents()
+	{
+		MOD_EVENT_BUS.register(ProxyClient.class);
+	}
 }
