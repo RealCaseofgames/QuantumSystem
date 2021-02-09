3a5341c2d92ed2b712de1d7af5ea2e1c73cdf7ab
Caseofgames <caseofgames@gmail.com>
2021-02-09 08:06:02 -0600

    First Block is finally added

--- src/main/java/caseofgames/quantumsystem/common/QSBlocks.java
+++ src/main/java/caseofgames/quantumsystem/common/QSBlocks.java
@@ -2,6 +2,7 @@ package caseofgames.quantumsystem.common;
 
 import caseofgames.quantumsystem.block.BlockSilicon;
 
+// Did I ever tell you that deferred registers are impossible to work with.
 public class QSBlocks
 {
 	public static BlockSilicon blocksilicon = new BlockSilicon();
