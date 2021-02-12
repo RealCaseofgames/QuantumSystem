1287570c5487af41a85ae974417d005d11d9b9ec
Caseofgames <caseofgames@gmail.com>
2021-02-11 22:40:24 -0600

    Added Silicon Ore to block database

--- src/main/java/caseofgames/quantumsystem/common/QSBlocks.java
+++ src/main/java/caseofgames/quantumsystem/common/QSBlocks.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import net.minecraft.block.Block;
 
 import caseofgames.quantumsystem.block.BlockSilicon;
+import caseofgames.quantumsystem.block.BlockSiliconOre;
 
 // Did I ever tell you that deferred registers are impossible to work with.
 public class QSBlocks
@@ -12,12 +13,14 @@ public class QSBlocks
 	public static ArrayList<Block> modBlocks = new ArrayList<>();
 	
 	public static BlockSilicon blocksilicon = new BlockSilicon();
+	public static BlockSiliconOre blocksiliconore = new BlockSiliconOre();
 	
 	public static void init()
 	{
 		modBlocks.add(blocksilicon);
+		modBlocks.add(blocksiliconore);
 		
-		// The initial size of ArrayList is 10. This reduces it to 1 to optimize the foreach loop
+		// The initial size of ArrayList is 10. This reduces it to 2 to optimize the foreach loop
 		modBlocks.trimToSize();
 	}
 }
